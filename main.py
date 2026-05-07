99094379c8d4da41d336911c3dae48d4c02f8437
TranHoangDatSC <hoangdattran12@gmail.com>
2026-05-08 00:59:45 +0700

    n

--- main.py
+++ main.py
@@ -4,17 +4,10 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# Gắn folder static
+# Phải khớp với tên thư mục static ở gốc
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Gắn folder templates
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-# Route dự phòng tránh lỗi 404
-@app.get("/{full_path:path}")
-async def catch_all(request: Request, full_path: str):
     return templates.TemplateResponse("index.html", {"request": request})
